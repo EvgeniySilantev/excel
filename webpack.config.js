44f0761f7f057fd19ca242d2c3e5d590606a007f
Evgeniy Silantev <silantev-zhenya@mail.ru>
2020-05-07 23:51:40 +0400

    Add plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,59 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpack = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    mode:'development',
+    mode: 'development',
     entry: './index.js',
     output: {
-        filename:'bundle.js',
-        path:path.resolve(__dirname,'dist')
+        filename: 'bundle.[hash:40].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@core': path.resolve(__dirname, 'src/core')
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyWebpack([
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist'),
+            }
+        ]),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash:40].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
     }
 }
